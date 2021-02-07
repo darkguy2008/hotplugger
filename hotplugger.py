091d6611520ebc962d62b2af0d6fb9f6b4c6fc49
DARKGuy <dark.guy.2008@gmail.com>
2021-02-07 14:13:02 -0600

    Small code refactor, fix for USB hubs only getting one device

--- hotplugger.py
+++ hotplugger.py
@@ -2,7 +2,6 @@
 import re
 import os
 import ast
-import fcntl
 import sys
 import yaml
 import json
@@ -11,25 +10,27 @@ import pprint
 from pathlib import Path
 from qemu import *
 
+configFilename = Path(__file__).parent / "config.yaml"
+tmpFolderPath = Path(__file__).parent / "tmp"
+
+
 def printp(dict):
 	pprint.pprint(dict, width=1)
+
 
 def sanitizeDevpath(devpath):
 	return devpath.replace('/', '_').replace(':', '_')
 
 
 def loadConfig():
-	path = Path(__file__).parent / "config.yaml"
-	with open(path) as file:
-		rv = yaml.load(file, Loader=yaml.FullLoader)
-	return rv
+	with open(configFilename) as file:
+		return yaml.load(file, Loader=yaml.FullLoader)
 
 
 def savePortDeviceMetadata(metadata, devpath):
-	tmpFolderPath = Path(__file__).parent / "tmp"
 	if not os.path.exists(tmpFolderPath):
 		os.makedirs(tmpFolderPath)
-	usbdefPath = Path(__file__).parent / f"tmp/{sanitizeDevpath(devpath)}"
+	usbdefPath = f"{tmpFolderPath}/{sanitizeDevpath(devpath)}"
 	print(f"Saving port metadata to {usbdefPath} ...")
 	f = open(usbdefPath, "w")
 	f.write(json.dumps(metadata))
@@ -37,7 +38,6 @@ def savePortDeviceMetadata(metadata, devpath):
 
 
 def updatePortDeviceMetadata(metadata, filename):
-	tmpFolderPath = Path(__file__).parent / "tmp"
 	if not os.path.exists(tmpFolderPath):
 		os.makedirs(tmpFolderPath)
 	print(f"Saving port metadata to {filename} ...")
@@ -53,10 +53,10 @@ def loadPortDeviceMetadata(config, devpath):
 				if devpath.find(port) >= 0:
 					print(f"Found {devpath} in port {port}")
 
-					tmpFolderPath = Path(__file__).parent / "tmp"
 					if not os.path.exists(tmpFolderPath):
 						os.makedirs(tmpFolderPath)
 					metadataFiles = [f for f in os.listdir(tmpFolderPath) if os.path.isfile(os.path.join(tmpFolderPath, f))]
+					metadataFiles.sort(key=len, reverse=True)
 					print(f"Metadata files:")
 					printp(metadataFiles)
 
@@ -88,12 +88,14 @@ def plug():
 		savePortDeviceMetadata(json.loads(json.dumps(dict(os.environ))), devpath)
 	else:
 		metadata = loadPortDeviceMetadata(config, devpath)
-		print(metadata)
+		if not metadata:
+			print(f"Metadata file for {devpath} not found")
+		else:
+			print(metadata)
 
-		qemu = QEMU()
-		qemu.connect(metadata["SOCKET"])
-		result = qemu.send({ "execute": "human-monitor-command", "arguments": { "command-line": "info usbhost" } })
-		usbhost = result['return']
+		print(f"Connecting to QEMU at {metadata['SOCKET']}...")
+		with QEMU(metadata["SOCKET"]) as qemu:
+			usbhost = qemu.hmp("info usbhost")
 		print(usbhost)
 
 		hostport = 0
@@ -113,12 +115,13 @@ def plug():
 		metadata["PORT"] = hostport
 		updatePortDeviceMetadata(metadata, metadata["FILENAME"])
 
-		if str(hostport) != "0":
+		print(f"Plugging USB device in port {hostport}...")
+		if hostport != '0':
 			time.sleep(1)
-			qemu.send({ "execute": "device_add", "arguments": { "driver": "usb-host", "hostbus": hostbus, "hostport": hostport, "id": f"device_BUS_{hostbus}_PORT_{hostport}_ADDR_{hostaddr}" } })
-			print("Device plugged in")
-
-		qemu.disconnect()
+			with QEMU(metadata["SOCKET"]) as qemu:
+				qemu.hmp(f"device_add driver=usb-host,hostbus={hostbus},hostport={hostport},id=device_BUS_{hostbus}_PORT_{hostport}_ADDR_{hostaddr}")
+				print("Device plugged in. Current USB devices on guest:")
+				print(qemu.hmp("info usb"))
 
 
 def unplug():
@@ -131,29 +134,32 @@ def unplug():
 	config = loadConfig()
 	devpath = os.environ['DEVPATH']
 	metadata = loadPortDeviceMetadata(config, devpath)
-	print(metadata)
+	if not metadata:
+		print(f"Metadata file for {devpath} not found")
+	else:
+		print(metadata)
 
 	is_usb_port = (os.getenv('DEVNUM') or '') != ''
 	print(f"Is USB Port? {is_usb_port}")
 
 	if is_usb_port == True:
-		qemu = QEMU()
-		qemu.connect(metadata["SOCKET"])
-		result = qemu.send({ "execute": "human-monitor-command", "arguments": { "command-line": "info usbhost" } })
-		usbhost = result['return']
+		print(f"Connecting to QEMU at {metadata['SOCKET']}...")
+		with QEMU(metadata["SOCKET"]) as qemu:
+			usbhost = qemu.hmp("info usbhost")
 		print(usbhost)
 
 		hostport = metadata['PORT']
 		hostaddr = metadata['DEVNUM'].lstrip('0')
-		hostbus = metadata['BUSNUM'].lstrip('0')		
+		hostbus = metadata['BUSNUM'].lstrip('0')
 		print(f"Found USB Bus: {hostbus}, Addr {hostaddr}, Port {hostport}")
 
 		time.sleep(1)
-		qemu.send({ "execute": "device_del", "arguments": { "id": f"device_BUS_{hostbus}_PORT_{hostport}_ADDR_{hostaddr}" } })
+		with QEMU(metadata["SOCKET"]) as qemu:
+			qemu.hmp(f"device_del device_BUS_{hostbus}_PORT_{hostport}_ADDR_{hostaddr}")
+
 		os.remove(metadata["FILENAME"])
 		print("Device unplugged")
 
-		qemu.disconnect()
 
 action = os.environ['ACTION']
 if action == 'add':
