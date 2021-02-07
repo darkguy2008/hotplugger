e0948d49210c5ec68bc84fba25ca5746a4e31771
DARKGuy <dark.guy.2008@gmail.com>
2021-02-07 14:12:26 -0600

    Improve / simplify QEMU helper lib

--- qemu.py
+++ qemu.py
@@ -1,19 +1,28 @@
 import json
+import fcntl
 import socket
+from pathlib import Path
 
 class QEMU:
 
 	client = None
+	socket = None
 
-	def __init__(self):
-		pass
 
-	def connect(self, unixSocket):
+	def __init__(self, unixSocket):
+		self.socket = unixSocket
+
+
+	def __enter__(self):
 		self.client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-		self.client.settimeout(.2)
-		self.client.connect(unixSocket)
+		self.client.connect(self.socket)
 		self.read(self.client)
-		self.send({ "execute": "qmp_capabilities" })
+		self.qmp({ "execute": "qmp_capabilities" })
+		return self
+
+
+	def __exit__(self, _type, value, tb):
+		self.client.close()
 
 
 	def read(self, socket):
@@ -27,13 +36,14 @@ class QEMU:
 				f += self.client.recv(1).decode()
 
 
-	def send(self, command):
+	def qmp(self, command):
 		message = f"{json.dumps(command)}\n"
 		print(f"SEND -> {message.strip()}")
 		self.client.send(str.encode(message))
 		return self.read(self.client)
 
 
-	def disconnect(self):
-		self.client.close()
+	def hmp(self, command):
+		result = self.qmp({ "execute": "human-monitor-command", "arguments": { "command-line": f"{command}" } })
+		return result['return'] if 'return' in result else result
 
